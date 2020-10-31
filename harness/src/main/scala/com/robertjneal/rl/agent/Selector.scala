3de438f8002c1f2afb34bbc3ea39c9a37b201d23
Robert J. Neal <robertjneal@users.noreply.github.com>
2020-10-31 08:13:21 -0700

    fix typed selector to work after type erasure (#26)

--- harness/src/main/scala/com/robertjneal/rl/agent/Selector.scala
+++ harness/src/main/scala/com/robertjneal/rl/agent/Selector.scala
@@ -3,15 +3,15 @@ package com.robertjneal.rl.agent
 import com.robertjneal.rl.types._
 import com.robertjneal.rl.types.goal._
 
-sealed trait Selector
+sealed trait Selector[A]
 
-case class RewardSelector(
+case class RewardSelector[A <: Reward](
     actionSelector: (
         Step,
         Map[State, Map[Action, Step]]
     ) => Map[Action, Reward] => Action
-) extends Selector
+) extends Selector[A]
 
-case class PreferenceSelector(
+case class PreferenceSelector[A <: Preference](
     actionSelector: Map[Action, Preference] => Action
-) extends Selector
+) extends Selector[A]
